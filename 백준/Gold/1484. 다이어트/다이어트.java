35a6bd39fd420ca0c04684e11b24635a3d80628f
JIHEE <69491404+bear1230@users.noreply.github.com>
2024-01-24 01:27:30 +0900

    solve : 다이어트 수정

--- 백준/Gold/1484. 다이어트/다이어트.java
+++ 백준/Gold/1484. 다이어트/다이어트.java
@@ -1,34 +1,34 @@
+package BOJ.week4.gold;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-public class Main {
-    public static void main(String[] args) throws NumberFormatException, IOException {
+/**
+ * N = 성원이 현재 몸무게 ^2 - 성원이가 기억하고 있던몸무게 ^2
+ * */
+public class BOJ_1484 {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        int start = 1;
-        int end = 1;
-        boolean flag = false;
-
-        while(true){
-            long ans = (long)(Math.pow(start, 2)) - (long)(Math.pow(end, 2));
-            if(start - end == 1 && ans > n)    break;
-            if(ans >= n)
-                end++;
-            else
-                start++;
-
-            if(ans == n){
-                System.out.println(start);
-                flag = true;
+        int N = Integer.parseInt(br.readLine());
+        long s = 1;
+        long e = 2;
+        StringBuilder sb =new StringBuilder();
+        while (e<100000){
+            long numS = s*s;
+            long numE = e*e;
+            if(numE-numS== N){
+                sb.append(e+"\n");
+            }
+            if(numE-numS>N){
+                s++;
+            }else {
+                e++;
             }
         }
+        if(sb.length()==0){
+           sb.append(-1);
+        }
+        System.out.println(sb);
 
-        if(!flag)
-            System.out.println(-1);
     }
 }
