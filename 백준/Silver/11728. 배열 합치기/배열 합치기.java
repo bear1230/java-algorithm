af16edd5f86163fad482d85728d06f8588e4c167
JIHEE <69491404+bear1230@users.noreply.github.com>
2024-01-24 00:55:43 +0900

    Solve : 배열 합치기 수정

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,42 +1,46 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.io.BufferedReader;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
+        int[] B = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        StringBuilder sb = new StringBuilder();
+        int numA = 0;
+        int numB = 0;
+        while (numA < N && numB < M) {
+            if (A[numA] < B[numB]) {
+                sb.append(A[numA] + " ");
+                numA++;
+            } else {
+                sb.append(B[numB] + " ");
+                numB++;
+            }
+        }
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int arrN[] = new int[n];
-		int arrM[] = new int[m];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<n;i++) {arrN[i] = Integer.parseInt(st.nextToken());}
-		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<m;i++) {arrM[i] = Integer.parseInt(st.nextToken());}
-		StringBuilder sb = new StringBuilder();
-		
-		int Nindex = 0;
-		int Mindex = 0;
-		
-		while(Nindex + Mindex < m+n) {
-			if(Nindex ==n) {
-				sb.append(arrM[Mindex++]+" ");
-			} else if(Mindex==m) {
-				sb.append(arrN[Nindex++]+" ");
-			} else {
-				if(arrM[Mindex] > arrN[Nindex]) {
-					sb.append(arrN[Nindex++]+" ");
-				} else {
-					sb.append(arrM[Mindex++]+" ");
-				}
-			}
-		}
-		System.out.println(sb.toString());
-			
-	}
+        while (numA < N) {
+            sb.append(A[numA] + " ");
+            numA++;
+        }
+        while (numB < M) {
+            sb.append(B[numB] + " ");
+            numB++;
+        }
+        System.out.println(sb);
 
+    }
 }
