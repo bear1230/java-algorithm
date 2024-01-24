b008b81b0f67937143854850453f646198043ffc
JIHEE <69491404+bear1230@users.noreply.github.com>
2024-01-24 20:51:21 +0900

    [Gold IV] Title: 회전 초밥, Time: 544 ms, Memory: 170876 KB -BaekjoonHub

---
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n,d,k,c;
+    //static int[] arr, vis;
+
+    public static void main(String[] args) throws Exception {
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        int arr[] = new int[n];
+        int vis[] = new int[d+1];
+
+        for (int i=0; i<n; i++)
+        arr[i] = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        
+        for (int i=0; i<k; i++) 
+            if (++vis[arr[i]]==1) cnt++;
+
+        int ans = cnt;
+        
+        for (int i=1; i<n; i++) {
+            if (--vis[arr[i-1]]==0) cnt--;
+            if (++vis[arr[(i+k-1)%n]]==1) cnt++;
+
+            if (ans<=cnt)
+                ans = vis[c]==0? cnt+1: cnt;
+        }
+        System.out.println(ans);
+    }
+}
