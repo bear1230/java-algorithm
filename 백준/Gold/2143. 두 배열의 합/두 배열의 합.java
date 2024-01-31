71da07d2dc568a14f2887063b46a16736a4e6b5f
JIHEE <69491404+bear1230@users.noreply.github.com>
2024-01-31 16:00:46 +0900

    [Gold III] Title: 두 배열의 합, Time: 644 ms, Memory: 114244 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -1,83 +1,63 @@
-import java.io.*;
-import java.util.*;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+/**
+ * 완탐 -> 시간초과
+ * 1. A배열의 부분합 모두 구하기, 횟수 기록 -> Map 사용
+ * 2. B배열의 부분합 모두 구하기, 횟수 기록
+ * 3. A배열 + B배열 중 합 T인 경우 세기
+ * **/
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int T = Integer.parseInt(br.readLine());
-
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N+1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int[] A = new int[n];
-        for(int i=0; i<n; i++) {
+        for(int i =1; i<=N; i++){
             A[i] = Integer.parseInt(st.nextToken());
         }
 
-        int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[] B = new int[M+1];
+        Map<Long, Long> sumA = new HashMap<>();
+        Map<Long, Long> sumB = new HashMap<>();
 
-        int[] B = new int[m];
-        for(int i=0; i<m; i++) {
+        st = new StringTokenizer(br.readLine());
+        for(int i =1; i<=M; i++){
             B[i] = Integer.parseInt(st.nextToken());
         }
 
-        List<Long> sumA = new ArrayList<>();
-        
-        for(int i=0; i<n; i++) {
-            long tmp = 0;
-            for(int j=i; j<n; j++) {
-                tmp += A[j];
-                sumA.add(tmp);
+
+        for(int i=1; i<=N; i++){
+            long sum =0;
+            for (int j =i; j<=N; j++){
+                sum+=A[j];
+                sumA.put(sum,sumA.getOrDefault(sum,0L)+1);
             }
         }
-        Collections.sort(sumA);
 
-        List<Long> sumB = new ArrayList<>();
-        for(int i=0; i<m; i++) {
-            long tmp = 0;
-            for(int j=i; j<m; j++) {
-                tmp += B[j];
-                sumB.add(tmp);
+
+        for(int i=1; i<=M; i++){
+            long sum =0;
+            for (int j =i; j<=M; j++){
+                sum+=B[j];
+                sumB.put(sum,sumB.getOrDefault(sum,0L)+1);
             }
         }
-        Collections.sort(sumB);
+        long cnt =0;
+        for(Map.Entry<Long,Long> entry: sumA.entrySet()){
+            long bKey = T-entry.getKey();
+            if(sumB.containsKey(bKey)){
+                cnt+= entry.getValue()*sumB.get(bKey);
+            }
 
-        long answer = 0;
-        for(int i=0; i<sumA.size(); i++) {
-            long target = T - sumA.get(i);
-            answer += upperBound(sumB, target) - lowerBound(sumB, target);
         }
-
-        System.out.println(answer);
-    }
-
-    private static int upperBound(List<Long> list, long target) {
-        int start = 0;
-        int end = list.size();
-
-        while(start < end) {
-            int mid = (start + end) / 2;
-            if(list.get(mid) <= target)
-                start = mid + 1;
-            else end = mid;
-        }
-
-        return end;
-    }
-
-    private static int lowerBound(List<Long> list, long target) {
-        int start = 0;
-        int end = list.size();
-
-        while(start < end) {
-            int mid = (start + end) / 2;
-            if(list.get(mid) >= target)
-                end = mid;
-            else start = mid + 1;
-        }
-
-        return end;
+        System.out.println(cnt);
     }
 }
