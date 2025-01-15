7d188b65c898c96f5b02d11fe87a0bc7b20d2f8e
gomdol <69491404+bear1230@users.noreply.github.com>
2025-01-16 01:11:00 +0900

    [Gold V] Title: 빗물, Time: 132 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -2,37 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/**
+ *
+ * 벽 높이 : [3, 1, 2, 3, 4, 1, 1, 2]
+ * 왼 -> 오
+ * 최대 높이 : [3, 3, 3, 3, 4, 4, 4, 4]
+ * 오-> 왼
+ * 최대 높이 : [4, 4, 4, 4, 4, 2, 2, 2]
+ * */
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         int H = Integer.parseInt(st.nextToken());
         int W = Integer.parseInt(st.nextToken());
-
-        int[] blocks = new int[W];
-        int[] rain = new int[W];
-        st = new StringTokenizer(br.readLine());
-
+        int[] left = new int[W];
+        int[] right = new int[W];
         int height = 0;
-        for (int i = 0; i < W; i++) {
-            blocks[i] = Integer.parseInt(st.nextToken());
-            height = Math.max(height, blocks[i]);
-            rain[i] = height;
-        }
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<W; i++){
+            left[i] = Integer.parseInt(st.nextToken());
+            height = Math.max(height,left[i]);
+            right[i] = height;
 
+        }
         height = 0;
-        int ans = 0;
-        for (int i = W - 1; i >= 0; i--) {
-            height = Math.max(height, blocks[i]);
-            rain[i] = Math.min(height, rain[i]);
-            ans += rain[i] - blocks[i];
+        int answer= 0;
+        for(int i =W-1; i>=0; i--){
+            height = Math.max(height,left[i]);
+            right[i] = Math.min(height,right[i]);
+            answer += right[i]- left[i];
         }
+        System.out.println(answer);
 
-        System.out.println(ans);
-        br.close();
     }
 }
