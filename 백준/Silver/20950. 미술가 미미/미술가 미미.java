70f1b3768d8997e599055b7d53b112e6bb8c0fc4
gomdol <69491404+bear1230@users.noreply.github.com>
2025-01-30 11:33:00 +0900

    [Silver II] Title: 미술가 미미, Time: 372 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/20950. 미술가 미미/미술가 미미.java
+++ 백준/Silver/20950. 미술가 미미/미술가 미미.java
@@ -1,66 +1,69 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * , 새로운 R 값은 혼합할 모든 물감의 R 값을 더한 후 이를 물감의 개수로 나누어 구한다. 이때 소수점은 버린다. G와 B 값도 동일한 방법으로 구한다.
- * <p>
- * 각 R G B 값을 각각 더해주고 N만큼 나눠준다 -> 문두리값
- * 물감은 2~7까지
- * ans += abs(곰두리 - 문두리)
- */
-public class Main {
-    static int n;
+public class Main{
+    static int N;
     static int[][] colors;
+    static boolean[] visited;
     static int R, G, B;
-    static int[] num;
     static int answer = Integer.MAX_VALUE;
-    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
-        n = Integer.parseInt(br.readLine());
-        colors = new int[n][3];
+        colors = new int[N][3];
         StringTokenizer st;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             colors[i][0] = Integer.parseInt(st.nextToken());
             colors[i][1] = Integer.parseInt(st.nextToken());
             colors[i][2] = Integer.parseInt(st.nextToken());
-
         }
-        num = new int[n];
+        visited = new boolean[N];
+
         st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
         G = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
-        int limit = Math.min(n, 7);
-        for (int i = 2; i <= limit; i++) {
-            recur(0, 0, i);
-        }
+        
+        recur(0, 0);
+        
         System.out.println(answer);
+
     }
 
-    private static void recur(int start, int cur, int count) {
-        if (cur == count) {
+    private static void recur(int cur, int cnt) {
+        if (cnt > 7) {
+            return;
+        }
+        
+        if (cur == N) {
+            if (cnt < 2) {
+                return;
+            }
+
             int r = 0;
             int g = 0;
             int b = 0;
-            for (int i = 0; i < count; i++) {
-                r += colors[num[i]][0];
-                g += colors[num[i]][1];
-                b += colors[num[i]][2];
+            for (int i = 0; i < N; i++) {
+                if (visited[i]) {
+                    r += colors[i][0];
+                    g += colors[i][1];
+                    b += colors[i][2];
+                }
             }
-            answer= Math.min(answer,Math.abs(R-r/count)+Math.abs(G-g/count)+Math.abs(B-b/count));
+            
+            answer = Math.min(answer, Math.abs(R - r / cnt) + Math.abs(G - g / cnt) + Math.abs(B - b / cnt));
             return;
         }
-        for (int i = start; i < n; i++) {
-            num[cur] = i;
-            recur(i + 1, cur + 1, count);
-        }
+        
+        visited[cur] = true;
+        recur(cur + 1, cnt + 1);
+        visited[cur] = false;
+        recur(cur + 1, cnt);
+
     }
 }
