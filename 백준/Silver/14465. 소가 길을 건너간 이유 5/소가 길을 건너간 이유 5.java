55cdde47858eaf9863fb02dd71e474b882eeaec7
gomdol <69491404+bear1230@users.noreply.github.com>
2025-02-04 11:48:39 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 116 ms, Memory: 14668 KB -BaekjoonHub

--- 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.java
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.java
@@ -3,49 +3,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /**
- * 횡단보도 N개
- * 연속한 신호등 k개
- * 고장난 시호등 B개
+ * 1번부터 N번까지의 번호가 붙은 횡단보도 N개
+ * <p>
+ * 연속한 K개의 신호등이 존재하도록 신호등 수리
+ * B줄 -> 고장난 신호등의 번호
  */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
 
-        boolean[] visited = new boolean[N+1];
+        boolean[] broken = new boolean[N + 1];
 
         for (int i = 0; i < B; i++) {
             int num = Integer.parseInt(br.readLine());
-            visited[num] = true;
+            broken[num] = true;
         }
 
-        
-        int count =0;
-        for (int i =1; i<=K; i++){
-            if(visited[i]){
-                count++;
-            }
-
+        int count = 0;
+        for (int i = 1; i <= K; i++) {
+            if (broken[i]) count++;
         }
-        int min =count;
-        for(int i=K+1;i<=N; i++){
-            if(visited[i-K]){
-                count--;
-            }
-            if (visited[i]){
-                count++;
-            }
-            min = Math.min(min,count);
+
+
+        int min = K;
+        int s = 1;
+        int e = K;
+        while (e < N) {
+            if (broken[s++]) count--;
+            if (broken[++e]) count++;
+
+            min = Math.min(count, min);
         }
 
         System.out.println(min);
+
 
     }
 }
