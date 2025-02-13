c1150978ff49d57e25b4272ad0d6cd3cf84086a1
gomdol <69491404+bear1230@users.noreply.github.com>
2025-02-13 11:50:05 +0900

    [Silver III] Title: N과 M (1), Time: 2044 ms, Memory: 51352 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * N과 M : 백트레킹
+ * 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+ *
+ * */
+public class Main {
+    static int N, M;
+    static int cur;
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N];
+        arr = new int[M];
+        cur = 0;
+        recur(cur);
+    }
+
+    private static void recur(int cur) {
+        if (cur == M) {  
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            visited[i] = true;
+            arr[cur] = i+1;
+            recur(cur + 1);
+            visited[i] = false;
+        }
+
+    }
+}
