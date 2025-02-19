e06905671d1fe5ee7846d99f6666698baa49077b
gomdol <69491404+bear1230@users.noreply.github.com>
2025-02-19 11:51:48 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 528 ms, Memory: 116836 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,46 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[][] arr;
-    static int N, M;
+    static int N,M;
     static boolean[] visited;
-    static int answer;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[][] map;
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); //정점
+        M = Integer.parseInt(st.nextToken()); // 간선
         
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        
-        arr = new int[N + 1][N + 1];
-        visited = new boolean[N + 1];
-        
-        for (int i = 0; i < M; i++) {
+        map = new int[N+1][N+1];
+        visited = new boolean[N+1];
+        for(int i =0; i<M; i++){
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            arr[a][b] = arr[b][a] = 1;
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            
+            map[x][y] = map[y][x] = 1;
         }
-
-        for (int i = 1; i <= N; i++) {
-            if (!visited[i]) {
+        int ans = 0;
+        
+        for(int i = 1; i<= N; i++){
+            //방문 x -> 정점 방문
+            if(!visited[i]){
                 dfs(i);
-                answer++;
+                ans++;
             }
         }
         
-        System.out.println(answer);
-    }
-
-    private static void dfs(int cur) {
-        visited[cur] = true;
+		System.out.print(ans);
         
-        for (int i = 1; i <= N; i++) {
-            if (!visited[i] && arr[cur][i] == 1) {
+    }
+    
+    public static void dfs(int n){
+        visited[n] = true;
+        for(int i =1; i <= N; i++){
+            if(map[n][i] == 1 && !visited[i]){
                 dfs(i);
             }
         }
