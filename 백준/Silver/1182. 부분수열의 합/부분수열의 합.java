9e0300a91a270a02c094fe9a73db133711a0cd5f
gomdol <69491404+bear1230@users.noreply.github.com>
2025-02-28 11:40:37 +0900

    [Silver II] Title: 부분수열의 합, Time: 112 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,43 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Inet4Address;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
+    static int N, S;
+    static int[] arr;
+    static int answer;
 
-	static int m;
-	static int check;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        recur(0, 0, 0);
+        System.out.println(answer);
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
+    }
 
-		int arr[] = new int[n];
-		m = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
-        
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-
-		dfs(0, 0, arr);
-        
-		if (m == 0) {
-			check = check - 1;
-		}
-		System.out.println(check);
-
-	}
-
-	public static void dfs(int n, int sum, int[] arr) {
-		if (sum == m) {
-			check++;
-		}
-		for (int i = n; i < arr.length; i++) {
-			dfs(i + 1, sum + arr[i], arr);
-		}
-	}
-
+    private static void recur(int cur, int cnt, int sum) {
+        if (cur == N) {
+            if (sum == S && cnt != 0) {
+                answer++;
+            }
+            return;
+        }
+        recur(cur + 1, cnt + 1, sum + arr[cur]);
+        recur(cur + 1, cnt, sum);
+    }
 }
