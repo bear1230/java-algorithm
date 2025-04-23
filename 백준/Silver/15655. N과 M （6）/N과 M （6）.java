189cd11ff1c8bc2663783355bb07e7d9214a1099
gomdol <69491404+bear1230@users.noreply.github.com>
2025-04-23 22:34:53 +0900

    [Silver III] Title: N과 M (6), Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,55 +1,50 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/**
- * N개의 자연수 중에서 M개를 고른 수열
- * 고른 수열은 오름차순이어야 한다.
- */
 public class Main {
     static int N, M;
     static int[] arr;
     static int[] answer;
-    static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
         arr = new int[N];
         answer = new int[M];
-        visited = new boolean[N];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
         Arrays.sort(arr);
-        recur(0, 0);
+        int cur = 0;
+        recur(cur, 0);
         System.out.println(sb);
+
     }
 
-    private static void recur(int s, int cur) {
+    private static void recur(int cur, int start) {
         if (cur == M) {
-            for (int num : answer) {
-                sb.append(num).append(" ");
+            for (int a : answer) {
+                sb.append(a).append(" ");
             }
-            sb.append("\n");
+            sb.append('\n');
             return;
-
         }
-        for (int i = s; i < N; i++) {
-            if (visited[i]) continue;
 
-            visited[i] = true;
+        for (int i = start; i < N; i++) {
             answer[cur] = arr[i];
-            recur(i+1,cur + 1);
-            visited[i] = false;
+            recur(cur + 1, i + 1);
         }
     }
 }
