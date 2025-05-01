965c2bc771722a6f04d6063922a852135372e455
gomdol <69491404+bear1230@users.noreply.github.com>
2025-05-01 18:45:13 +0900

    [Gold IV] Title: N-Queen, Time: 5852 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,52 +1,55 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 /**
- * 체스룰로 퀸은 상하좌우, 대각선, 거리제한 없이 한 방향으로 이동 가능.
- * 퀸이 놓여져 있는부분에 이동할 수 있는 방향은 X표시, 남는 칸에만 퀸 놓을 수 있음
- */
+ * 퀸은 대각선, 수평선으로 이동 가능
+ * 퀸 N개를 서로 공격할 수 없는 위치에 놓기.
+ * 0,0에 퀸 있으면 x+1,0 0,y+1, x+1. y+1 증가하는곳에 놓을수 없음
+ * */
 public class Main {
-    static int[] map;
     static int N;
-    static int count;
+    static int[] board;
+    private static int count = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        map = new int[N];
 
-        dfs(0);
+        board = new int[N];
+        recur(0);
         System.out.println(count);
 
     }
 
-    private static void dfs(int depth) {
-        if (depth == N) {
+    private static void recur(int cur) {
+
+        if (cur == N) {
             count++;
             return;
         }
 
         for (int i = 0; i < N; i++) {
-            map[depth] = i;
-            if (possible(depth)) {
-                dfs(depth + 1);
+            board[cur] = i;
+            //cur열에서 i번쨰 행에 놓을 수 있는지 체크
+            if (check(cur)) {
+                recur(cur+1);
             }
+
         }
     }
 
-    private static boolean possible(int col) {
-        for (int i = 0; i < col; i++) {
-            //같은 행인지 체크
-            if(map[i] == map[col]){
+    //cur: 열, 값 : 행
+    private static boolean check(int cur) {
+        for (int i = 0; i < cur; i++) {
+            if (board[cur] == board[i]) {
                 return false;
             }
 
-            //대각선 ㅊ ㅔ크
-            else if (Math.abs(col-i)== Math.abs(map[col]-map[i])) {
+            //열의 차이 == 행의 차이 같은 대각선
+            if (Math.abs(cur - i) == Math.abs(board[cur] - board[i])) {
                 return false;
-
             }
         }
         return true;
