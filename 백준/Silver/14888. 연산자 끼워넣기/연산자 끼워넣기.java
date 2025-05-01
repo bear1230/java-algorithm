21f779b7601625e530df6809f9948d90d7a76a21
gomdol <69491404+bear1230@users.noreply.github.com>
2025-05-01 16:21:03 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 108 ms, Memory: 15692 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,70 +1,71 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
- * N개의 수로 이루어진 수열 A1, A2, ..., AN
- * 수와 수 사이에 끼워넣을 수 있는 N-1개의 연산자가 주어진다. 연산자는 덧셈(+), 뺄셈(-), 곱셈(×), 나눗셈(÷)
- * <p>
- * 식의 계산은 연산자 우선 순위를 무시하고 앞에서부터 진행해야 한다. 또, 나눗셈은 정수 나눗셈으로 몫만 취한다.
- * 즉, 양수로 바꾼 뒤 몫을 취하고, 그 몫을 음수로 바꾼 것과 같다.
- * <p>
- * 덧셈(+), 뺄셈(-), 곱셈(×), 나눗셈(÷)
- */
+ * 덧셈2, 뺄셈1, 곱셈1, 나눗셈1
+ *
+ *
+ * */
 public class Main {
     static int N;
     static int[] arr;
-    static int[] oper;// 0 : + , 1 :-, 2: x, 3 : ÷
-    static int MIN = Integer.MAX_VALUE;
-    static int MAX = Integer.MIN_VALUE;
+    static int[] sign;
+    static int min = Integer.MAX_VALUE;
+    static int max = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
         arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        oper = new int[4];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 4; i++) {
-            oper[i] = Integer.parseInt(st.nextToken());
-        }
-        recur(arr[0], 1);
-        System.out.println(MAX);
-        System.out.println(MIN);
 
+        sign = new int[4];
+
+        st = new StringTokenizer(br.readLine());
+        // 덧샘 뺄셈 곱셈 나눗셈
+        for (int i = 0; i < 4; i++) {
+            sign[i] = Integer.parseInt(st.nextToken());
+        }
+
+        recur(1, arr[0]);
+        System.out.println(max);
+        System.out.println(min);
     }
 
-    private static void recur(int sum, int cur) {
-        if (cur ==N){
-            MAX = Math.max(MAX,sum);
-            MIN = Math.min(MIN,sum);
+    private static void recur(int cur, int sum) {
+        if (cur == N) {
+            max = Math.max(max, sum);
+            min = Math.min(min, sum);
             return;
         }
-            for (int i = 0; i < 4; i++) {
-                if (oper[i] > 0) {
-                    oper[i]--;
-                    switch (i) {
-                        case 0:
-                            recur(sum + arr[cur], cur + 1);
-                            break;
-                        case 1:
-                            recur(sum - arr[cur], cur + 1);
-                            break;
-                        case 2:
-                            recur(sum * arr[cur], cur + 1);
-                            break;
-                        case 3:
-                            recur(sum / arr[cur], cur + 1);
-                            break;
-                    }
-                    // 재귀호출이 종료되면 다시 해당 연산자 개수를 복구
-                    oper[i]++;
+
+        for (int i = 0; i < 4; i++) {
+            if (sign[i] > 0) {
+                sign[i]--;
+                switch (i) {
+                    case 0:
+                        recur(cur + 1, sum  + arr[cur] );
+                        break;
+                    case 1:
+                        recur(cur + 1, sum - arr[cur]);
+                        break;
+                    case 2:
+                        recur(cur + 1, sum * arr[cur]);
+                        break;
+                    case 3:
+                        recur(cur + 1, sum / arr[cur] );
+                        break;
                 }
+                sign[i]++;
             }
+
+        }
+
     }
 }
