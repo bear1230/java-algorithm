82aa15a96eade4309d64cff0db6fd7ca71217210
gomdol <69491404+bear1230@users.noreply.github.com>
2025-05-06 13:40:58 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -5,37 +5,41 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static int[] sour;
-    static int[] bitter;
-    static int answer = Integer.MAX_VALUE;
+    static int[] S;
+    static int[] B;
+    static int min = Integer.MAX_VALUE;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        
         N = Integer.parseInt(br.readLine());
-        sour = new int[N];
-        bitter = new int[N];
-        for (int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            sour[i] = Integer.parseInt(st.nextToken());
-            bitter[i] = Integer.parseInt(st.nextToken());
+
+        S = new int[N];
+        B = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            S[i] = Integer.parseInt(st.nextToken());
+            B[i] = Integer.parseInt(st.nextToken());
         }
 
-        recur(0, 0, 1,0);
-        
-        System.out.println(answer);
+        //신맛은 곱이기 때문에 1
+        recur(0, 0, 1, 0);
+
+        System.out.println(min);
     }
 
-    private static void recur(int cur, int count, int s, int b)  {
-        if (cur == N){
-            if(count!=0){
-                answer = Math.min(answer,Math.abs(s-b));
+    private static void recur(int cur, int cnt, int sNum, int bNum) {
+        if (cur == N) {
+            if (cnt != 0) {
+                int sub = Math.abs(sNum - bNum);
+                min = Math.min(min, sub);
             }
-            
             return;
         }
 
-        recur(cur+1,count+1,sour[cur]*s, bitter[cur]+b);
-        recur(cur+1,count,s,b);
+        //재료를 고른경우
+        recur(cur + 1, cnt + 1, sNum * S[cur], bNum + B[cur]);
+        //안고른 경우
+        recur(cur + 1, cnt, sNum, bNum);
     }
 }
