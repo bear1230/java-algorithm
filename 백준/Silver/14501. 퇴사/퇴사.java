2cae2fed376ecc972880a2127df206fa36a6a42a
gomdol <69491404+bear1230@users.noreply.github.com>
2025-05-14 16:16:58 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -3,38 +3,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//기간 T, 금액 P
 public class Main {
     static int N;
     static int[][] arr;
-    static int answer;
+    static int max = Integer.MIN_VALUE;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
+
+
         arr = new int[N][2];
-        for(int i=0; i<N; i++) {
+
+
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken()); 
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][0] = Integer.parseInt(st.nextToken()); // 소요기간
+            arr[i][1] = Integer.parseInt(st.nextToken()); // 금액
         }
 
         recur(0, 0);
-
-        System.out.println(answer);
+        System.out.println(max);
     }
 
-    static void recur(int cur, int pay) {
-        if(cur >= N) {
-            answer = Math.max(pay, answer);
+    private static void recur(int cur, int cnt) {
+        if (cur > N) {
             return;
         }
 
-        if(cur + arr[cur][0] <= N) { 
-            recur(cur + arr[cur][0], pay + arr[cur][1]);
-        } else { 
-            recur(cur + arr[cur][0], pay);
+        if (cur == N) {
+            max = Math.max(max, cnt);
+            return;
         }
-        
-        recur(cur + 1, pay);
+
+        recur(cur + arr[cur][0], cnt + arr[cur][1]);
+        recur(cur + 1, cnt);
     }
 }
